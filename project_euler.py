93ebc456ae8221df57ae0ac539144390dffc023a
Albert <albertjozwiak1@gmail.com>
2020-03-29 22:01:10 +0200

    solved problem 6

--- project_euler.py
+++ project_euler.py
@@ -59,16 +59,28 @@
 # 5 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
-num = 20
-finish = False
-while True:
-    for i in range(1, 21):
-        if num % i != 0:
-            break
-        if i == 20:
-            finish = True
-    if finish == True:
-        break
-    print(num)
-    num += 20
-print(f'final {num}')
+# num = 20
+# finish = False
+# while True:
+#     for i in range(1, 21):
+#         if num % i != 0:
+#             break
+#         if i == 20:
+#             finish = True
+#     if finish == True:
+#         break
+#     print(num)
+#     num += 20
+# print(f'final {num}')
+
+#6 The sum of the squares of the first ten natural numbers is,
+# 12+22+...+102=385
+# The square of the sum of the first ten natural numbers is,
+# (1+2+...+10)2=552=3025
+# Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025−385=2640.
+# Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+sum_of_squares = sum([n**2 for n in range(101)])
+square_of_sum = sum([n for n in range(101)])**2
+
+print(square_of_sum-sum_of_squares)
