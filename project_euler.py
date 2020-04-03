cc6b4a33979f6ceb33931bd9d084987b23d706da
Albert <albertjozwiak1@gmail.com>
2020-04-03 23:33:51 +0200

    solved problem 7

--- project_euler.py
+++ project_euler.py
@@ -73,14 +73,36 @@
 #     num += 20
 # print(f'final {num}')
 
-#6 The sum of the squares of the first ten natural numbers is,
+# 6 The sum of the squares of the first ten natural numbers is,
 # 12+22+...+102=385
 # The square of the sum of the first ten natural numbers is,
 # (1+2+...+10)2=552=3025
 # Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025−385=2640.
 # Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 
-sum_of_squares = sum([n**2 for n in range(101)])
-square_of_sum = sum([n for n in range(101)])**2
+# sum_of_squares = sum([n**2 for n in range(101)])
+# square_of_sum = sum([n for n in range(101)])**2
 
-print(square_of_sum-sum_of_squares)
+# print(square_of_sum-sum_of_squares)
+
+
+# 7 By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+# What is the 10 001st prime number?
+
+
+def look_for_prime_2(position_to_look_for=10001):
+    start = time.time()
+
+    number = 3
+    current_prime_position = 2
+
+    while current_prime_position != position_to_look_for:
+        number += 1
+        finish = int(number/2)
+        for i in range(2, finish+1):
+            if number % i == 0:
+                break
+            if i == finish:
+                current_prime_position += 1
+                break
+    return number
